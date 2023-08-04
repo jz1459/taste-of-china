bb983f935b0c80013725db3b71fa00cde68628af
Jason Zheng <jason.zheng@yale.edu>
2023-08-03 22:02:53 -0400

    Added embeded google maps

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -70,6 +70,9 @@ function Contact() {
                                 </ul> */}
                             </Col>
                         </Row>
+                        <Row>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11656.928835217395!2d-73.8180686393977!3d43.0786132447614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de38306cbb1511%3A0x2704c36c25a2a838!2sTaste%20of%20CHINA!5e0!3m2!1sen!2sus!4v1691114539171!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </Row>
                     </div>
                 </div>
             </Container>
